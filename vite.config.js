a79563934aa53af906cabcb9d12af8546e64e0b2
drizzy765 <agorotimilehin05@gmail.com>
2025-12-21 18:03:49 +0100

    Fix build: externalize node modules

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'fs', // Exclude file system module
+        'path', // Exclude path module
+        'os', // Exclude os module
+      ],
+    },
+  },
 })
