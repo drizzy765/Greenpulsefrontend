f593406b522f048bb9c1378f63c89638c11d4970
drizzy765 <agorotimilehin05@gmail.com>
2025-12-21 18:45:27 +0100

    Security: Move firebase config to env vars

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIRu9FELlH-EQvcCOr-UqvGDOeKtj6zzQ",
-    authDomain: "greenpulseng-ed97c.firebaseapp.com",
-    projectId: "greenpulseng-ed97c",
-    storageBucket: "greenpulseng-ed97c.firebasestorage.app",
-    messagingSenderId: "893779141442",
-    appId: "1:893779141442:web:f19a558aec78af0296dedb",
-    measurementId: "G-90DYX5EX86"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
